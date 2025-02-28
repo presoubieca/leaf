52ae564b8c545a1dc03f78a40277c2dfddba0968
presoubieca <106857284+presoubieca@users.noreply.github.com>
2025-02-28 23:52:19 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,10 +1,5 @@
 @echo off
-setlocal enabledelayedexpansion
-
-
-start "" "http://127.0.0.1:5000"
-
-echo Starting....
-python flask1.py
-
-echo here
+echo Starting Python server...
+start /B python flask1.py
+timeout /t 20 /nobreak >nul 2>&1
+start http://127.0.0.1:5000
